dd043aa80789f6025d7806c7c5abbe72702e8fe9
Shrishailya Sutar <shreesutar96@gmail.com>
2021-04-27 12:35:23 +0530

    loginpg2

--- app/src/main/java/com/example/user/Login.java
+++ app/src/main/java/com/example/user/Login.java
@@ -2,13 +2,38 @@ package com.example.user;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
+    private Button loginbt;
+    private TextView forgetpass;
+    private EditText emailEdittxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginbt=(Button)findViewById(R.id.loginbtn);
+        forgetpass=(TextView)findViewById(R.id.ForgetPasswordtxt);
+        emailEdittxt=(EditText)findViewById(R.id.emailedittxt);
+        loginbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(emailEdittxt.getText().toString().equals("iotproject488@gmail.com"))
+                    startActivity(new Intent(Login.this,OwnerHome.class));
+                else
+                    startActivity(new Intent(Login.this,bluetooth.class));
+            }
+        });
+        forgetpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this,ForgetPassword.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/user/OwnerHome.java
+++ app/src/main/java/com/example/user/OwnerHome.java
@@ -2,13 +2,44 @@ package com.example.user;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OwnerHome extends AppCompatActivity {
-
+    private Button setRate,addcustomer,custinfo,logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_home);
+        setRate=(Button)findViewById(R.id.homeSetRatebtn);
+        addcustomer=(Button)findViewById(R.id.homeAddCustomerbtn);
+        custinfo=(Button)findViewById(R.id.homeCustomerInfobtn);
+        logout=(Button)findViewById(R.id.homeLogoutbtn);
+        setRate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OwnerHome.this,setNewRate.class));
+            }
+        });
+        addcustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OwnerHome.this,Registration.class));
+            }
+        });
+        custinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OwnerHome.this,CustomerInformation.class));
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OwnerHome.this,Login.class));
+            }
+        });
     }
 }
