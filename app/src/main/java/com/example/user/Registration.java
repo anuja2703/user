1471c575f5a196218f3401f9ff3e7953e4e49434
Shrishailya Sutar <shreesutar96@gmail.com>
2021-04-28 22:48:50 +0530

    loginpg2

--- app/src/main/java/com/example/user/Registration.java
+++ app/src/main/java/com/example/user/Registration.java
@@ -1,14 +1,122 @@
 package com.example.user;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Registration extends AppCompatActivity {
-
+    private EditText etname,etmno,etmail,etpass;
+    private Button register;
+    private FirebaseAuth mAuth;
+    private TextView goback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        goback=findViewById(R.id.regGoBack);
+        mAuth=FirebaseAuth.getInstance();
+        register=findViewById(R.id.regbtn);
+        etname=findViewById(R.id.regNametxt);
+        etmno=findViewById(R.id.regMobiletxt);
+        etmail=findViewById(R.id.regEmailTxt);
+        etpass=findViewById(R.id.editTextTextPassword);
+        register=findViewById(R.id.regbtn);
+        goback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Registration.this,MainActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String name=etname.getText().toString();
+                final String email=etmail.getText().toString();
+                final String mno=etmno.getText().toString();
+                String pass=etpass.getText().toString();
+                if(name.isEmpty())
+                {
+                    etname.setError("Enter the name");
+                    etname.requestFocus();
+                    return;
+                }
+                if(mno.isEmpty())
+                {
+                    etmno.setError("Enter mobile number");
+                    etmno.requestFocus();
+                    return;
+                }
+                if(email.isEmpty())
+                {
+                    etmail.setError("Enter email address");
+                    etmail.requestFocus();
+                    return;
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                {
+                    etmail.setError("Enter valid email");
+                    etmail.requestFocus();
+                    return;
+                }
+                if(pass.isEmpty())
+                {
+                    etpass.setError("Enter the password");
+                    etpass.requestFocus();
+                    return;
+                }
+                if(pass.length()<6)
+                {
+                    etpass.setError("Minimum password length should be 6 character");
+                    etpass.requestFocus();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email,pass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful())
+                                {
+                                    User user=new User(name,mno,email);
+                                    FirebaseDatabase.getInstance().getReference("Users")
+                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful())
+                                            {
+                                                Toast.makeText(Registration.this,"User has been registered successfully",Toast.LENGTH_SHORT).show();
+                                            }
+                                            else {
+                                                Toast.makeText(Registration.this,"Failed to register! Try again!",Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                                }
+                                else{
+                                    Toast.makeText(Registration.this,"Failed to register! Try again!",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/user/User.java
@@ -0,0 +1,14 @@
+package com.example.user;
+
+public class User {
+
+    public String name,mno,email;
+    public User(){
+
+    }
+    public User(String name, String mno, String email){
+        this.name=name;
+        this.mno=mno;
+        this.email=email;
+    }
+}
