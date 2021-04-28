e11ec338840e527d5574c494b380db5b7da32918
Shrishailya Sutar <shreesutar96@gmail.com>
2021-04-28 22:58:15 +0530

    loginpg2

--- app/src/main/java/com/example/user/ForgetPassword.java
+++ app/src/main/java/com/example/user/ForgetPassword.java
@@ -1,14 +1,69 @@
 package com.example.user;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgetPassword extends AppCompatActivity {
-
+    private TextView back;
+    private Button reset;
+    private EditText email;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+        back=findViewById(R.id.ForgetPasswordGoLogin);
+        reset=findViewById(R.id.ForgetPasswordbtn);
+        email=findViewById(R.id.ForgetPasswordEmailtxt);
+        mAuth=FirebaseAuth.getInstance();
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail=email.getText().toString().trim();
+                if(mail.isEmpty())
+                {
+                    email.setError("Enter email address");
+                    email.requestFocus();
+                    return;
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(mail).matches())
+                {
+                    email.setError("Enter valid email");
+                    email.requestFocus();
+                    return;
+                }
+                mAuth.sendPasswordResetEmail(mail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(ForgetPassword.this,"Check your email to reste password",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(ForgetPassword.this,"Something went wrong! please try again",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ForgetPassword.this,MainActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/user/Login.java
+++ app/src/main/java/com/example/user/Login.java
@@ -1,38 +1,88 @@
 package com.example.user;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
-    private Button loginbt;
-    private TextView forgetpass;
-    private EditText emailEdittxt;
+    private TextView forpass;
+    private Button btnsignin;
+    private EditText etmail,etpass;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginbt=(Button)findViewById(R.id.loginbtn);
-        forgetpass=(TextView)findViewById(R.id.forgetpasswordtxt);
-        emailEdittxt=(EditText)findViewById(R.id.emailedittxt);
-        loginbt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(emailEdittxt.getText().toString().equals("iotproject488@gmail.com"))
-                    startActivity(new Intent(Login.this,OwnerHome.class));
-                else
-                    startActivity(new Intent(Login.this,MainActivity.class));
-            }
-        });
-        forgetpass.setOnClickListener(new View.OnClickListener() {
+        forpass=findViewById(R.id.forgetpasswordtxt);
+        btnsignin=findViewById(R.id.loginbtn);
+        etmail=findViewById(R.id.emailedittxt);
+        etpass=findViewById(R.id.passedittxt);
+        mAuth=FirebaseAuth.getInstance();
+        forpass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Login.this,ForgetPassword.class));
+            }
+        });
+
+        btnsignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userlogin();
+            }
+        });
+
+
+    }
+
+    private void userlogin() {
+        final String email=etmail.getText().toString().trim();
+        String pass=etpass.getText().toString().trim();
+        if(email.isEmpty())
+        {
+            etmail.setError("Please enter email");
+            etmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            etmail.setError("Please enter valid email");
+            etmail.requestFocus();
+            return;
+        }
+        if(pass.isEmpty()) {
+            etpass.setError("Please enter password");
+            etpass.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    if(email.equals("iotproject488@gmail.com"))
+                        startActivity(new Intent(Login.this,OwnerHome.class));
+                    else
+                        startActivity(new Intent(Login.this,MainActivity.class));
+                }
+                else
+                {
+                    Toast.makeText(Login.this,"Failed to login! Please, check your login credentials",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
